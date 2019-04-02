0befec0136a072748a18dbb8ff091df999633a00
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-02 17:26:09 +0200

    fight with sword right done movement done. Duck right movement done.

--- classes.js
+++ classes.js
@@ -2,19 +2,13 @@
 const theCanvas = document.getElementById('screengame');
 const context = theCanvas.getContext('2d');
 
-// Setting background
+// Setting background from DOM
 var background = document.getElementById('screengame');
-
-//Creating background image
-// var background = new Image();
-// background.src = './Images/background.png';
-// background.onload;
 
 //=== VARIABLES DEFINITION ===
 //Coordenates background start
 const backX = 0;
 const backY = 0;
-var movement = 15; //amount to move background
 var rightPressed = false;
 var leftPressed = false;
 // var upPressed = false;
@@ -25,12 +19,14 @@ var audio = new Audio('./Audio/02 - Main Theme - The Real Ghostbusters (DECO8) -
 audio.play();
 
 
+// To know the original position of the background image
 var backgroundPosY = document.getElementById('screengame').offsetLeft;
 console.log(backgroundPosY);
 var backgroundPosX = document.getElementById('screengame').offsetTop;
 console.log(backgroundPosX);
 
 
+// === ARTHUR OBJECT === //
 const arthur = {
   srcX: 0,
   srcY: 0,
@@ -43,7 +39,7 @@ const arthur = {
   colums: 4,
   rows: 10,
   currentFrame: 0,
-
+  // Seting initial positions and states of Arthur
   stand: true,
   goRight: false,
   left: false,
@@ -52,29 +48,32 @@ const arthur = {
   jumpRight: false,
   jumpLeft: false,
   duckRight: false,
-  duckLeft: false,
+  // duckLeft: false,
   atkRight: false,
   atkLeft: false,
   dieRight: false,
-
+  //To know the number of the row wher the movement is in the sprite
   standRowRight: 0,
   goRowRight: 1,
   goRowLeft: 2,
   jumpRowRight: 3,
   jumpRowLeft: 4,
   duckRowRight: 5,
-  duckRowLeft: 6,
+  // duckRowLeft: 6,
   atkRowRight: 7,
   atkRowLeft: 8,
   dieRowRight: 9,
 }
 
+//draw arthur
 var arthurImage = new Image();
 arthurImage.src = './Images/arthur_true.png';
 arthurImage.onload = function () {
   context.drawImage(arthurImage, arthur.x, arthur.y, arthur.width, arthur.height)
 }
 
+
+//function to update de animation
 function updateFrame() {
   arthur.currentFrame = ++arthur.currentFrame % arthur.colums;
   arthur.srcX = arthur.currentFrame * arthur.width;
@@ -82,7 +81,7 @@ function updateFrame() {
   context.clearRect(arthur.x, arthur.y, 64, 64);
 }
 
-
+// frunction to draw the loop
 function drawArthur() {
   updateFrame();
   animations();
@@ -90,37 +89,46 @@ function drawArthur() {
   context.drawImage(arthurImage, arthur.srcX, arthur.srcY, 64, 64, arthur.x, arthur.y, arthur.width, arthur.height);
 }
 
-
+// Set interval of animations
 setInterval(() => {
   drawArthur();
 }, 100);
 
 
+// Function that set animations depending of the state of the object Arthur
 function animations() {
   arthur.srcX = arthur.currentFrame * arthur.width;
   if (arthur.goRight) {
     arthur.srcY = arthur.goRowRight * arthur.height;
   } else if (arthur.goLeft) {
     arthur.srcY = arthur.goRowLeft * arthur.height;
+  } else if (arthur.duckRight) {
+    arthur.srcY = arthur.duckRowRight * arthur.height;
+  } else if (arthur.atkRight) {
+    arthur.srcY = arthur.atkRowRight * arthur.height;
   }
 }
 
+// function that reset all states every time in the draw loop to set the states at the origin state
 function resetAnimations() {
   arthur.stand = arthur.right;
   arthur.goLeft = false;
   arthur.goRight = false;
+  arthur.duckRight = false;
+  arthur.atkRight = false;
   // knight.jumpLeft = false;
   // knight.jumpRight = false;
 }
 
 
 
-var bgPos = 0;
+var bgPos = 0; // variable that indicates the pixels to move the background. Initial 0
 window.onkeydown = function (event) {
   console.log(event);
 
+  //Swith statement to know what key is pressed and to change the states with this
   switch (event.keyCode) {
-    case 39:
+    case 39: //right key
       console.log("yay", event)
       event.preventDefault();
       if (bgPos > (-6140)) {
@@ -132,7 +140,7 @@ window.onkeydown = function (event) {
       arthur.left = false;
       console.log(arthur.goRight)
       break;
-    case 37:
+    case 37: //left key
       console.log('left mode ON', event);
       event.preventDefault();
       if (bgPos != 0) {
@@ -142,16 +150,21 @@ window.onkeydown = function (event) {
       arthur.goLeft = true;
       arthur.left = true;
       arthur.right = false;
-
       break;
-    case 40:
+    case 40: //down key
       console.log('sitting down!!');
+      event.preventDefault();
+      arthur.right = true;
+      arthur.duckRight = true;
       break;
-    case 32:
+    case 32: // jumping key
       console.log('Im not going to jump now!');
       break;
-    case 65:
+    case 65: //fight key
       console.log('Im ready, ¡FIGHT!');
+      event.preventDefault();
+      arthur.right = true;
+      arthur.atkRight = true;
       break;
     default:
       break;
